d9b9683c2b52a21d180afa589b2607d9e64a263a
Dan Phillips <dlphillips@gmail.com>
2017-04-27 12:23:58 -0400

    added parameter for Twitter

--- liri.js
+++ liri.js
@@ -14,8 +14,6 @@ var client = new twitter({
 var liriCmd = process.argv[2];
 var liriPara = process.argv[3];
 
-var req = [];
-
 var getRandom = function() {
     return new Promise(function(resolve, reject) {
         fs.readFile('random.txt', 'utf8', function(error, data) {
@@ -33,19 +31,13 @@ var getCall = function(message) {
     });
 };
 
-
-// if (liriCmd === 'do-what-it-says') {
-//     getRandom().then(function(p1, p2) {
-//         return getCall(p1, p2);
-//     })
-// } else {
-//     doSomething(liriCmd, liriPara);
-// }
-
-
 switch (liriCmd) {
     case 'my-tweets':
-        doSomething(liriCmd);
+        if (liriPara === undefined) {
+            doSomething(liriCmd, "dlphillips");
+        } else {
+            doSomething(liriCmd, liriPara);
+        }
         break;
     case 'spotify-this-song':
         if (liriPara === undefined) {
@@ -72,18 +64,23 @@ function doSomething(p1, p2) {
 
     switch (p1) {
         case 'my-tweets':
-            var params = { screen_name: 'dlphillips' };
+            var params = { screen_name: p2 };
             client.get('statuses/user_timeline', params, function(error, tweets, response) {
                 if  (error)  {        
                     console.log('Error occurred: '  +  error);        
                     return;    
-                } 
-                console.log('======================');
-                for (var i = 0; i < tweets.length; i++) {
-                    console.log(tweets[i].text);
-                    console.log(tweets[i].created_at);
                 }
-                console.log('======================');
+                console.log("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+                console.log('============*** 20 most recent Tweets for ' + params.screen_name + ' ***=============');
+                console.log(' ');
+                for (var i = 0; i < tweets.length; i++) {
+                    console.log('--------Tweet #' + (i + 1) + ' - Sent ' + tweets[i].created_at + '--------');
+                    console.log(tweets[i].text);
+                    console.log(' ');
+                }
+                console.log('--------------------------------------------------------------------');
+                console.log('|"my-tweets" accepts a parameter too!  Try a diffent twitter user. |');
+                console.log('--------------------------------------------------------------------');
             });
             break;
         case 'spotify-this-song':
@@ -91,35 +88,41 @@ function doSomething(p1, p2) {
                 if  (error)  {        
                     console.log('Error occurred: '  +  error);        
                     return;    
-                } 
-                console.log('======================');
-                console.log(data.tracks.items[0].album.artists[0].name);
-                console.log(data.tracks.items[0].name);
-                console.log(data.tracks.items[0].preview_url);
-                console.log(data.tracks.items[0].album.name);
-                console.log('======================');
+                }
+                console.log("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+                console.log(' ');
+                console.log('===*** Song Search for ' + p2 + ' ***===');
+                console.log("Artist Name: " + data.tracks.items[0].album.artists[0].name);
+                console.log("Track Name: " + data.tracks.items[0].name);
+                console.log("Preview URL: " + data.tracks.items[0].preview_url);
+                console.log("Album Name: " + data.tracks.items[0].album.name);
+                console.log('=================================================');
+                console.log(' ');
             });
             break;
         case 'movie-this':
-            var queryUrl = "http://www.omdbapi.com/?t=" + p2 + "&y=&plot=short&r=json";
+            var queryUrl = "https://www.omdbapi.com/?t=" + p2 + "&type=movie&tomatoes=true";
             request(queryUrl, function(error, response, body) {
                 if  (error)  {        
                     console.log('Error occurred: '  +  error);        
                     return;    
                 } 
-                var rtUrl = "https://www.rottentomatoes.com/m/"+p2.split(' ').join('_')+"/";
-                var rYear = JSON.parse(body).Released;
-                console.log('======================');
-                console.log("Title: " + JSON.parse(body).Title);
-                console.log("Year Released: " + rYear.slice(-4));
-                console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
-                console.log("Produced in: " + JSON.parse(body).Country);
-                console.log("Language: " + JSON.parse(body).Language);
-                console.log("Plot: " + JSON.parse(body).Plot);
-                console.log("Actors: " + JSON.parse(body).Actors);
-                console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
-                console.log("Rotten Tomatoes URL: "+ rtUrl);
-                console.log('======================');
+                var movieObj = JSON.parse(body);
+                console.log(' ');
+                console.log("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+                console.log('====*** Movie Search for ' + p2 + ' ***====');
+                console.log("Title: " + movieObj.Title);
+                console.log("Year Released: " + movieObj.Year);
+                console.log("IMDB Rating: " + movieObj.imdbRating);
+                console.log("Produced in: " + movieObj.Country);
+                console.log("Language: " + movieObj.Language);
+                console.log("Plot: " + movieObj.Plot);
+                console.log("Actors: " + movieObj.Actors);
+                console.log("Rotten Tomatoes Rating: " + movieObj.Ratings[1].Value);
+                console.log("Rotten Tomatoes URL: " + movieObj.tomatoURL);
+                console.log('==================================================');
+                console.log(' ');
+
             });
             break;
     }
